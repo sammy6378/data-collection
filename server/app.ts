ab51fa580d1d7877aa82cf44aaba03e5c3148cc3
sammy6378 <samuel202mwangi@gmail.com>
2025-08-01 01:08:42 +0300

    feat: Update allowedOrigins in CORS configuration to include production URL

--- server/app.ts
+++ server/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:5173", "https://data-collection-ruby.vercel.app"];
 
 app.use(
   cors({
